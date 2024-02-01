3a527558f6042aa172d5bcb052d879a2e96e9d5b
“Pilar <rodr802485@siigo.com>
2024-02-01 16:59:45 -0500

    Validar caracteres distintos a numeros enteros

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Calcula 2");
+        Scanner teclado = new Scanner(System.in);
+        int numero1 = 0;
+        int numero2 = 0;
+        boolean esEntero = false;
+        do {
+            try {
+
+                System.out.println("PIDE UN NUMERO");
+                numero1 = Integer.parseInt(teclado.next());
+                esEntero = true;
+
+
+            } catch (NumberFormatException error) {
+                System.out.println("El número debe ser entero");
+                esEntero = false;
+            }
+        } while (esEntero == false);
+        do {
+            try {
+
+                System.out.println("ESCRIBE OTRO NUMERO");
+                numero2 = Integer.parseInt(teclado.next());
+                esEntero = true;
+
+
+            } catch (NumberFormatException error) {
+                System.out.println("El número debe ser entero");
+                esEntero = false;
+            }
+        } while (esEntero == false);
     }
 }
